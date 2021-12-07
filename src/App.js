42b773267af58527bee1dd836dba83e8bc7622dd
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-12-08 00:55:20 +0530

    added pre-loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useState, useEffect } from "react";
 import Grid from "./components/Grid";
+import HashLoader from "react-spinners/HashLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
-    <div>
-      <h1>My work</h1>
-      <Grid />
-    </div>
+    <>
+      {loading ? (
+        <div className="preloader">
+          <HashLoader color={"#3f3fff"} loading={loading} size={150} />
+        </div>
+      ) : (
+        <div>
+          <h1>My work</h1>
+          <Grid />
+        </div>
+      )}
+    </>
   );
 }
 
