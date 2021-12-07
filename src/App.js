e7888763e0f0aefb8f70c06fc3d91b67ffba38e9
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-12-07 09:32:18 +0530

    added images, poppins font, and created card and grid component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Grid from "./components/Grid";
+
 function App() {
   return (
     <div>
       <h1>Projects</h1>
+      <Grid />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+export default function Card() {
+  return (
+    <div className="img-container">
+      <div className="content">
+        <section>
+          <h2>style points</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+            voluptate necessitatibus, iste assumenda nemo vel voluptatibus
+            deserunt fugiat dolore id atque officia sit, est reprehenderit
+            error, perspiciatis ea ex aperiam.
+          </p>
+        </section>
+        <section>
+          <h2>amazing stuff</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi
+            voluptate necessitatibus, iste assumenda nemo vel voluptatibus
+            deserunt fugiat dolore id atque officia sit, est reprehenderit
+            error, perspiciatis ea ex aperiam.
+          </p>
+        </section>
+      </div>
+      <div className="cursor"></div>
+    </div>
+  );
+}
