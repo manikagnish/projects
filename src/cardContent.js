3e78f21dfca45fab38971cb3d581a3a5eda11a41
manikagnish <72827130+manikagnish@users.noreply.github.com>
2022-01-21 14:49:25 +0530

    updated projects

--- src/cardContent.js
+++ src/cardContent.js
@@ -1,15 +1,22 @@
 export const cardContent = [
   {
     id: 1,
-    title: "blogr landing page",
-    thumbnail: "./images/blogr.jpg",
+    title: "music events app",
+    thumbnail: "./images/music_events.png",
     description:
-      "frontendmentor challenge, Blogr landing page using ReactJs and SCSS.",
-    github: "https://github.com/manikagnish/blogr-landing-page",
-    website: "https://manikagnish.com/blogr-landing-page/",
+      "An application to find the nearest music events using Next js, strapi backend and jwt authentication.",
+    github: "https://github.com/manikagnish/music-events-frontend",
   },
   {
     id: 2,
+    title: "netflix clone",
+    thumbnail: "./images/netflix-clone.png",
+    description:
+      "Built a Netflix clone using react js, react router, styled components using the tmdb api. ",
+    github: "https://github.com/manikagnish/netflix-clone",
+  },
+  {
+    id: 3,
     title: "hivecoin",
     thumbnail: "./images/hivecoin.png",
     description:
@@ -18,7 +25,16 @@ export const cardContent = [
     website: "https://manikagnish.com/hivecoin/",
   },
   {
-    id: 3,
+    id: 4,
+    title: "blogr landing page",
+    thumbnail: "./images/blogr.jpg",
+    description:
+      "frontendmentor challenge, Blogr landing page using ReactJs and SCSS.",
+    github: "https://github.com/manikagnish/blogr-landing-page",
+    website: "https://manikagnish.com/blogr-landing-page/",
+  },
+  {
+    id: 5,
     title: "natours",
     thumbnail: "./images/natours.png",
     description:
@@ -27,7 +43,7 @@ export const cardContent = [
     website: "https://manikagnish.com/natours/",
   },
   {
-    id: 4,
+    id: 6,
     title: "blog site",
     thumbnail: "./images/blog-site.png",
     description:
@@ -36,16 +52,35 @@ export const cardContent = [
     website: "https://manikagnish.com/blog/",
   },
   {
-    id: 5,
-    title: "sneakers",
-    thumbnail: "./images/sneakers.jpg",
+    id: 7,
+    title: "country-search-app",
+    thumbnail: "./images/country-search-app.jpg",
     description:
-      "Product page for an online sneaker store using reactjs, styled-components and context api.",
-    github: "https://github.com/manikagnish/ecommerce-product-page",
-    website: "https://manikagnish.com/ecommerce-product-page/",
+      "REST Countries API with color theme switcher challenge hub. Using react.js, axios and tailwind.",
+    github: "https://github.com/manikagnish/country-search-app",
+    website: "https://manikagnish.com/country-search-app/",
   },
   {
-    id: 6,
+    id: 8,
+    title: "todo-list-app",
+    thumbnail: "./images/todo-list-app.jpg",
+    description:
+      "Created a todo list app using cra-5, context api, useReducer, beautiful dnd, localStorage and tailwind.",
+    github: "https://github.com/manikagnish/todo-list-app",
+    website: "https://manikagnish.com/todo-list-app",
+  },
+  {
+    id: 9,
+    title: "speed type",
+    thumbnail: "./images/speedtype.png",
+    description:
+      "Built a speed typing app using react js, custom hooks and quotable api.",
+    github: "https://github.com/manikagnish/speed-typing",
+    website:
+      "https://61e70417331b82009bc75e50--vigorous-hoover-eabdff.netlify.app/",
+  },
+  {
+    id: 10,
     title: "breakout",
     thumbnail: "./images/breakout.png",
     description:
@@ -54,7 +89,7 @@ export const cardContent = [
     website: "https://manikagnish.com/breakout/",
   },
   {
-    id: 7,
+    id: 11,
     title: "theme switcher",
     thumbnail: "./images/themeswitcher.jpg",
     description:
@@ -63,25 +98,7 @@ export const cardContent = [
     website: "https://manikagnish.com/theme-switcher/",
   },
   {
-    id: 8,
-    title: "country search app",
-    thumbnail: "./images/country-search-app.jpg",
-    description:
-      "REST Countries API with color theme switcher challenge hub. Using react.js, axios and tailwind.",
-    github: "https://github.com/manikagnish/country-search-app",
-    website: "https://manikagnish.com/country-search-app",
-  },
-  {
-    id: 9,
-    title: "todo list app",
-    thumbnail: "./images/todo-list-app.jpg",
-    description:
-      "Created a todo list app using cra-5, context api, useReducer, beautiful dnd, localStorage and tailwind.",
-    github: "https://github.com/manikagnish/todo-list-app",
-    website: "https://manikagnish.com/todo-list-app/",
-  },
-  {
-    id: 10,
+    id: 12,
     title: "huddle landing page",
     thumbnail: "./images/huddle.jpg",
     description:
@@ -90,12 +107,20 @@ export const cardContent = [
     website: "https://manikagnish.com/huddle-landing-page",
   },
   {
-    id: 11,
+    id: 13,
     title: "tic tac toe",
-    thumbnail: "./images/themeswitcher.jpg",
+    thumbnail: "./images/tictactoe.png",
     description:
       "A social media dashboard with an ability to switch theme using react styled-components and context api.",
     github: "https://github.com/manikagnish/ticTacToe",
     website: "https://manikagnish.com/ticTacToe/",
+  },
+  {
+    id: 14,
+    title: "pong game",
+    thumbnail: "./images/pong-game.png",
+    description: "Buit a pong game with vanilla javascript.",
+    github: "https://github.com/manikagnish/pong-game",
+    website: "https://manikagnish.com/pong-game",
   },
 ];
